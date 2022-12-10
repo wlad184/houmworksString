c8c6a9aa28204684871f88ee63b6899416fd5314
wlad184 <evel184@yandex.ru>
2022-12-10 11:44:31 +0700

    ДЗ Строки.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
+        //task #1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullsName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullsName);
+        System.out.println();
+
+
+        //task #2
+        String fullName = "Ivanov Ivan Ivanovich";
+        String fullNameUpper = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameUpper);
+        System.out.println();
+
+        //task #3
+        String fullName1 = "Иванов Семён Семёнович";
+        String fullName2 = fullName1.replace('ё', 'e');
+        System.out.println("Данные ФИО сотрудника — " + fullName2);
+        System.out.println();
     }
 }
